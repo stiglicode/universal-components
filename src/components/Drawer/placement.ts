f7da0c921292d267728041fcd841277f5cb7c434
Milan Štiglic <stiglic.work@gmail.com>
2022-03-19 17:54:58 +0100

    Added direction detector & Added resizer position

--- src/components/Drawer/placement.ts
+++ src/components/Drawer/placement.ts
@@ -1,46 +1,98 @@
 import { css, FlattenSimpleInterpolation } from "styled-components";
-import { IPlacement } from "../../types/global";
+import { IPlacement } from "../../types/drawer";
 
 export const drawerPlacement = (
   placement: IPlacement,
-  size: number
+  initSize: number,
+  afterResize: number
 ): FlattenSimpleInterpolation => {
   const verticalPlacement = css`
     left: 0;
     right: 0;
-    height: ${size}px;
+    min-height: ${initSize}px;
   `;
   const horizontalPlacement = css`
     top: 0;
     bottom: 0;
-    width: ${size}px;
+    min-width: ${initSize}px;
   `;
 
   switch (placement) {
   case "top":
     return css`
-        top: -${size}px;
+        top: -${afterResize}px;
         ${verticalPlacement}
       `;
   case "bottom":
     return css`
-        bottom: -${size}px;
+        bottom: -${afterResize}px;
         ${verticalPlacement}
       `;
   case "left":
     return css`
-        left: -${size}px;
+        left: -${afterResize}px;
         ${horizontalPlacement}
       `;
   case "right":
     return css`
-        right: -${size}px;
+        right: -${afterResize}px;
         ${horizontalPlacement}
       `;
   default:
     return css`
-        right: -${size}px;
+        right: -${afterResize}px;
         ${horizontalPlacement}
+      `;
+  }
+};
+
+export const detectDirection = (
+  placement: IPlacement
+): "vertical" | "horizontal" => {
+  if (placement === "top" || placement === "bottom") {
+    return "vertical";
+  } else {
+    return "horizontal";
+  }
+};
+
+export const resizerPosition = (
+  placement: IPlacement,
+  size: number
+): FlattenSimpleInterpolation => {
+  const verticalPlacement = css`
+    width: ${size}px;
+    height: 100%;
+  `;
+  const horizontalPlacement = css`
+    width: 100%;
+    height: ${size}px;
+  `;
+  switch (placement) {
+  case "top":
+    return css`
+        bottom: 0;
+        ${horizontalPlacement}
+      `;
+  case "bottom":
+    return css`
+        top: 0;
+        ${horizontalPlacement}
+      `;
+  case "left":
+    return css`
+        right: 0;
+        ${verticalPlacement}
+      `;
+  case "right":
+    return css`
+        left: 0;
+        ${verticalPlacement}
+      `;
+  default:
+    return css`
+        left: 0;
+        ${verticalPlacement}
       `;
   }
 };
