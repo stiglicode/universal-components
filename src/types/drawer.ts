fbe7f1eb983f230c42456ba9f7edb794a627e607
Milan Štiglic <stiglic.work@gmail.com>
2022-03-19 17:53:47 +0100

    Added resize types & Moved here IPlacement type

--- src/types/drawer.ts
+++ src/types/drawer.ts
@@ -1,12 +1,21 @@
-import { IComponentWithMask, IPlacement, These, UCN } from "./global";
+import { IComponentWithMask, These, UCN } from "./global";
 
 export interface DrawerProps
   extends These<Partial<StyledDrawerProps> & IComponentWithMask> {
-  resizeable?: boolean;
+  resizerClass?: string;
+  onResize?: (position: number) => void;
+  onResizeStart?: (position: number) => void;
+  onResizeEnd?: (position: number) => void;
+  cachedResizeSize?: boolean;
 }
 
 export interface StyledDrawerProps extends UCN {
   size: number;
+  readonly afterResize?: number;
   placement: IPlacement;
   disableSmoothness: boolean;
+  resizeable?: boolean;
+  resizerSize: number;
 }
+
+export type IPlacement = "left" | "right" | "top" | "bottom";
