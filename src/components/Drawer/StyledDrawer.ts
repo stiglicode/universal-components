cd8acfdec8d64eb958847c8ac21157a98879afae
Milan Štiglic <stiglic.work@gmail.com>
2022-03-19 17:56:17 +0100

    Added resizer class & Added on exit state

--- src/components/Drawer/StyledDrawer.ts
+++ src/components/Drawer/StyledDrawer.ts
@@ -1,19 +1,22 @@
 import styled, { css } from "styled-components";
 import { defaultSettings } from "../../settings";
-import { drawerPlacement } from "./placement";
+import { detectDirection, drawerPlacement, resizerPosition } from "./placement";
 import { StyledDrawerProps } from "../../types/drawer";
 
 export const StyledDrawer = styled.div<StyledDrawerProps>(
-  ({ size, placement, disableSmoothness }) => {
+  ({ size, placement, disableSmoothness, afterResize, resizerSize }) => {
     return css`
       position: absolute;
       inset: 0;
       display: flex;
+      pointer-events: none;
 
       ${!disableSmoothness &&
       css`
         &.enter-done,
         &.enter-active {
+          pointer-events: all;
+
           .${defaultSettings.maskClassPrefix("drawer")} {
             opacity: 1;
           }
@@ -21,6 +24,10 @@ export const StyledDrawer = styled.div<StyledDrawerProps>(
           .${defaultSettings.wrapperClassPrefix("drawer")} {
             ${placement}: 0;
           }
+        }
+
+        &.exit-done {
+          pointer-events: none;
         }
       `}
       .${defaultSettings.maskClassPrefix("drawer")} {
@@ -36,11 +43,19 @@ export const StyledDrawer = styled.div<StyledDrawerProps>(
 
       .${defaultSettings.wrapperClassPrefix("drawer")} {
         position: absolute;
-        ${drawerPlacement(placement, size)}
+
+        ${drawerPlacement(placement, size, afterResize || size)}
         ${!disableSmoothness &&
         css`
-          transition: ${defaultSettings.duration}ms, width 0s;
+          transition: ${defaultSettings.duration}ms, width 0s, height 0s;
         `}
+        &_resizer {
+          position: absolute;
+          cursor: ${detectDirection(placement) === "horizontal"
+    ? "ew-resize"
+    : "ns-resize"};
+          ${resizerPosition(placement, resizerSize)}
+        }
       }
     `;
   }
